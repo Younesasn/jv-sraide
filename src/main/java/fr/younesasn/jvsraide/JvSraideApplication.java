15a1865ed0cba9abdb11f171e517c815e7bdef6b
ASSANI Younes <younesassani@gmail.com>
2025-03-09 13:54:46 +0100

    first commit

---
+++ src/main/java/fr/younesasn/jvsraide/JvSraideApplication.java
@@ -0,0 +1,13 @@
+package fr.younesasn.jvsraide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvSraideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvSraideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/younesasn/jvsraide/JvSraideApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.younesasn.jvsraide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvSraideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
